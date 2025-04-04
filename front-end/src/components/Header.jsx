c8d1fe9cd03a1bf16816bdc1fdda66bd838711cb
Felipe <fs754282@gmail.com>
2025-04-04 16:02:38 -0300

    back-end do login feito e front-end da page register feito

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../img/pngegg.png"; // Importando a imagem
 
-const Header = () => {
+const Header = ({ user }) => {
   return (
     <header className="shadow-md">
       <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-4 sm:px-8">
         <Link to="/" className="flex items-center">
-          <img className="h-[68px] w-[120px]" src={logo} alt="Logo" />
+          <img
+            className="h-10"
+            src="https://cdn.prod.website-files.com/61b9e0dd381626819c8d4f83/65e2198d48039ba6444f602b_logo%20hashtag%20-%20h.webp"
+            alt="Logo da Hashtag"
+          />
+          <p className="text-primary-400 text-2xl font-bold">ashbnb</p>
         </Link>
 
-        <Link to="/" className="hidden items-center rounded-full border border-gray-300 py-2 pr-4 pl-6 shadow-md lg:flex">
+        <Link
+          to="/"
+          className="hidden items-center rounded-full border border-gray-300 py-2 pr-4 pl-6 shadow-md lg:flex"
+        >
           <p className="border-r border-r-gray-300 pr-4">Qualquer lugar</p>
           <p className="border-r border-r-gray-300 px-4">Qualquer semana</p>
           <p className="px-4">Hóspedes</p>
+
           <div className="bg-primary-400 rounded-full p-2 text-white">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -32,7 +40,10 @@ const Header = () => {
           </div>
         </Link>
 
-        <Link to='/login' className="flex items-center gap-2 rounded-full border border-gray-300 py-2 pr-4 pl-6 shadow-md">
+        <Link
+          to={user ? "/account" : "/login"}
+          className="flex items-center gap-2 rounded-full border border-gray-300 py-2 pr-4 pl-6 shadow-md"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
@@ -59,7 +70,11 @@ const Header = () => {
             />
           </svg>
 
-          <p className="max-w-20 truncate sm:max-w-32">Felipe SSB</p>
+          {user ? (
+            <p className="max-w-20 truncate sm:max-w-32">{user.name}</p>
+          ) : (
+            <></>
+          )}
         </Link>
       </div>
     </header>
